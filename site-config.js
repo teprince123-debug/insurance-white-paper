d85f4f8f59d0eb5dd1d473c49a7e513542fb176f
Pravven <teprince123@gmail.com>
2026-09-05 11:31:25 -0400

    Connect Mailchimp signup form to the Pravven audience

--- site-config.js
+++ site-config.js
@@ -15,8 +15,8 @@ window.PRAVVEN_CONFIG = {
        2. the hidden anti-bot input's NAME     -> mailchimpBotField
           (it looks like: b_1a2b3c4d5e6f7_a1b2c3d4e5)
      ---------------------------------------------------------------------- */
-  mailchimpAction:   "",
-  mailchimpBotField: "",
+  mailchimpAction:   "https://carrd.us11.list-manage.com/subscribe/post?u=5e2a16e99d30d34a948166280&id=fc57c3ec39&f_id=00958fe0f0",
+  mailchimpBotField: "b_5e2a16e99d30d34a948166280_fc57c3ec39",
 
   /* ---- Contact form ------------------------------------------------------
      Any no-backend form endpoint works (Formspree, Basin, Formsubmit...).
